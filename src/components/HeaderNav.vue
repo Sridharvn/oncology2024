8a16375ac1b3935a4815e75af67787d9b76739c1
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-02-01 14:15:40 +0530

    Added brochure download

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- http://www.oncology2024.in/brochure-2024.pdf -->
+
   <div class="nav">
     <div class="nav-logo">
       <a href="/"> <img src="/ccrs-logo.png" alt="" class="nav-logo-img" /></a>
@@ -15,6 +17,11 @@
       </div>
       <div class="nav-items">
         <a href="/SteeringCommittee">Steering Committee</a>
+      </div>
+      <div class="nav-items">
+        <a href=" http://www.oncology2024.in/brochure-2024.pdf" target="_blank"
+          >Download Brochure</a
+        >
       </div>
     </div>
   </div>
@@ -33,7 +40,7 @@
 
 .nav-links {
   @apply flex gap-5 text-white font-bold justify-center items-center text-center;
-  @apply max-sm:flex max-sm:text-xs max-sm:gap-2 max-sm:justify-center;
+  @apply max-sm:flex max-sm:text-xs max-sm:gap-2 max-sm:justify-center max-sm:flex-wrap;
 }
 .nav-items {
   @apply bg-text-primary2 rounded-md p-2;
