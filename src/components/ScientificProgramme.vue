7828d00a824f21da4cfe2afd528f523dadc48ee9
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-02-03 01:01:52 +0530

    3 days programmes added

--- src/components/ScientificProgramme.vue
+++ src/components/ScientificProgramme.vue
@@ -1,5 +1,121 @@
-<template>Hello</template>
-<script lang="ts" setup>
+<template>
+  <div class="wrapper">
+    <div class="dayWrapper">
+      <h1 class="heading">Scientific Sessions - Day I - 08.03.2024(Hall A)</h1>
+      <h6 class="underHeading">
+        There maybe minor changes in the Scientific Programme.
+      </h6>
+      <table class="listWrapper">
+        <tr class="listHeading">
+          <th class="listHeadingItem">Topic</th>
+          <th class="listHeadingItem">Speaker</th>
+        </tr>
+
+        <tr
+          class="listItems"
+          v-for="(item, index) in sessionsDay1"
+          :key="index"
+        >
+          <td class="listItemsItem">{{ item.topic }}</td>
+          <td class="listItemsItem">{{ item.speaker }}</td>
+        </tr>
+      </table>
+    </div>
+    <div class="dayWrapper">
+      <h1 class="heading">Scientific Sessions - Day II - 09.03.2024(Hall A)</h1>
+      <h6 class="underHeading">
+        There maybe minor changes in the Scientific Programme.
+      </h6>
+      <table class="listWrapper">
+        <tr class="listHeading">
+          <th class="listHeadingItem">Topic</th>
+          <th class="listHeadingItem">Speaker</th>
+        </tr>
+
+        <tr
+          class="listItems"
+          v-for="(item, index) in sessionsDay2"
+          :key="index"
+        >
+          <td class="listItemsItem">{{ item.topic }}</td>
+          <td class="listItemsItem">{{ item.speaker }}</td>
+        </tr>
+      </table>
+    </div>
+    <div class="dayWrapper">
+      <h1 class="heading">
+        Scientific Sessions - Day III - 10.03.2024(Hall A)
+      </h1>
+      <h6 class="underHeading">
+        There maybe minor changes in the Scientific Programme.
+      </h6>
+      <table class="listWrapper">
+        <tr class="listHeading">
+          <th class="listHeadingItem">Topic</th>
+          <th class="listHeadingItem">Speaker</th>
+        </tr>
+
+        <tr
+          class="listItems"
+          v-for="(item, index) in sessionsDay3"
+          :key="index"
+        >
+          <td class="listItemsItem">{{ item.topic }}</td>
+          <td class="listItemsItem">{{ item.speaker }}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+<style scoped>
+* {
+  /* border: 1px solid red; */
+}
+.dayWrapper {
+  @apply border border-text-primary rounded-lg m-2 border-4;
+}
+.heading {
+  @apply text-text-primary text-3xl px-4 pt-4 text-center;
+}
+.underHeading {
+  @apply text-text-primary text-sm pt-1 px-4 pb-4 italic text-center border-b-2;
+}
+.listHeading {
+  /* display: grid;
+  grid-template-columns: repeat(2, 1fr); */
+  /* grid-template-rows: ; */
+  /* grid-column-gap: 130px;
+  grid-row-gap: 20px; */
+  /* margin-left: 180px; */
+  /* padding-block: 10px; */
+}
+.listHeadingItem {
+  @apply text-text-primary text-3xl  underline text-left px-4;
+}
+.listItems {
+  /* display: grid;
+  grid-template-columns: repeat(2, 1fr); */
+  /* grid-template-rows: ; */
+  /* grid-column-gap: 200px;
+  grid-row-gap: 20px; */
+  /* margin-left: 10%; */
+  /* padding-block: 10px; */
+}
+.listItemsItem {
+  text-align: left;
+  @apply text-text-primary text-base py-2 px-4;
+}
+.listItemsWrapper {
+}
+.listWrapper {
+  @apply max-w-6xl;
+}
+
+.wrapper {
+  @apply flex justify-center flex-wrap flex-auto;
+}
+</style>
+<script setup>
 const dailyEventsTable = [
   {
     date: "07 March ",
@@ -341,5 +457,11 @@ const sessionsDay3 = [
   //     topic: "",
   //     speaker: "",
   //   },
+];
+const precisionOncology8thMarch = [
+  {
+    topic: "",
+    speaker: "",
+  },
 ];
 </script>
