a2fb5793c44a17616184dfa5a242215a1f30dabb
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-02-06 21:28:05 +0530

    Changed Color Scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,12 @@ module.exports = {
 	theme: {
 		extend: {},
 		colors: {
-			'text-primary': '#403092',
-			'text-primary2': '#6a6296',
-			'text-primary3': '#22194d',
+			'text-primary-old': '#403092',
+			'text-primary': '#58216f',
+			'text-primary2-old': '#6a6296',
+			'text-primary2': '#8846a3',
+			'text-primary3-old': '#22194d',
+			'text-primary3': '#48145e',
 			'text-secondary': "#a491c3",
 			'text-secondary2': "#ffffff75",
 			'border-blue': "#c9c1f7",
